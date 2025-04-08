c05b43819798edfa738382832f7b364df14e833c
unknown <blackskiesdancer@gmail.com>
2025-04-08 22:04:57 +1000

    updated choresData

--- src/data/choresData.js
+++ src/data/choresData.js
@@ -1,7 +1,7 @@
 export const chores = {
-    kitchen: ['Wash dishes', 'Clean counters', 'Take out trash'],
-    bathroom: ['Scrub toilet', 'Clean mirror', 'Mop floor'],
-    bedroom: ['Make bed', 'Dust furniture', 'Vacuum carpet'],
-    livingRoom: ['Organize books', 'Dust shelves', 'Sweep floor'],
+    kitchen: ['Wash dishes', 'Clean counters', 'Take out trash', 'Organise Fridge'],
+    bathroom: ['Scrub toilet', 'Spray anti-mould', 'Clean Shower', 'Wipe basin'],
+    bedroom: ['Make bed', 'Dust furniture', 'Remove rubbish', 'Put away laundry'],
+    livingRoom: ['Organize books', 'Dust surfaces', 'Put blankets away'],
   };
   
